a4455c928fbb040a0fd758b51f46319ee51265b5
Jonathan Hanska Susanto <jojo12ku@gmail.com>
2026-04-29 23:53:32 +0700

    feat: add middleware for authentication

---
+++ backend/src/middleware/auth.middleware.ts
@@ -0,0 +1,54 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+import { JwtPayload } from '../types/express';
+import { handleError } from '../utils/request.util';
+
+export function authenticate(req: Request, res: Response, next: NextFunction): void {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        res.status(401).json({
+            status: 'fail',
+            message: 'Access denied. No token provided',
+            data: null,
+            error: null
+        });
+        return;
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload;
+
+        req.jwtPayload = {
+            id: decoded.id,
+            role: decoded.role
+        };
+
+        next();
+    } catch (error) {
+        const err = handleError(error)
+        console.log('Error registering user: ', err)
+        res.status(401).json({
+            status: 'fail',
+            message: 'Invalid or expired token',
+            data: null,
+            error: err.message
+        });
+    }
+}
+
+export function authorizeAdmin(req: Request, res: Response, next: NextFunction): any {
+    if (!req.jwtPayload || req.jwtPayload.role !== 'ADMIN') {
+        return res.status(403).json({
+            status: 'fail',
+            message: 'Access denied. Admin privileges required',
+            data: null,
+            error: null
+        });
+    }
+
+    next();
+}
--- backend/src/types/express.d.ts
+++ backend/src/types/express.d.ts
@@ -1,9 +1,15 @@
 import { AuthUser } from "./auth.types";
 
+export interface JwtPayload {
+    id: string
+    role: string
+}
+
 declare global{
     namespace Express{
         interface Request{
             user?: AuthUser;
+            jwtPayload?: JwtPayload;
         }
     }
 }
