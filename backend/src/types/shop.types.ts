971d0d65d462affd53a351b33fbcd6cd6d082917
Jonathan Hanska Susanto <jojo12ku@gmail.com>
2026-04-30 23:57:09 +0700

    feat: add shop types

---
+++ backend/src/types/shop.types.ts
@@ -0,0 +1,102 @@
+export interface ShopWeapon {
+    weaponId: number
+    elementId: number
+    name: string
+    type: string
+    description: string
+    stock: number
+    imageUrl: string
+    price: number | string
+    damage: number
+    element?: {
+        name: string
+        type: string
+    }
+    createdAt: Date
+    createdBy: string | null
+    updatedAt: Date
+    updatedBy: string | null
+}
+
+export interface ShopArtifact {
+    artifactId: number
+    elementId: number
+    name: string
+    type: string
+    description: string
+    stock: number
+    imageUrl: string
+    price: number | string
+    primaryStat: number | string
+    secondaryStat: number | string
+    element?: {
+        name: string
+        type: string
+    }
+    createdAt: Date
+    createdBy: string | null
+    updatedAt: Date
+    updatedBy: string | null
+}
+
+export interface CreateWeaponInput {
+    name: string
+    type: string
+    description: string
+    elementId: number
+    stock: number
+    imageUrl: string
+    price: number
+    damage: number
+}
+
+export interface CreateArtifactInput {
+    name: string
+    type: string
+    description: string
+    elementId: number
+    stock: number
+    imageUrl: string
+    price: number
+    primaryStat: number
+    secondaryStat: number
+}
+
+export interface UpdateWeaponInput {
+    name?: string
+    type?: string
+    description?: string
+    elementId?: number
+    stock?: number
+    imageUrl?: string
+    price?: number
+    damage?: number
+}
+
+export interface UpdateArtifactInput {
+    name?: string
+    type?: string
+    description?: string
+    elementId?: number
+    stock?: number
+    imageUrl?: string
+    price?: number
+    primaryStat?: number
+    secondaryStat?: number
+}
+
+export interface InventoryItem {
+    inventoryId: number
+    itemId: number
+    itemType: string
+    name: string
+    imageUrl: string
+}
+
+export interface PurchaseResult {
+    purchaseId: number
+    userId: string
+    itemId: number
+    itemType: string
+    money: number | string
+}
