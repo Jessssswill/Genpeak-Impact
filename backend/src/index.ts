b180465e01989f6ba66d2c2e7828a73c2e596f71
Jonathan Hanska Susanto <jojo12ku@gmail.com>
2026-05-02 16:12:50 +0700

    feat: add admin endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,27 @@
 import express from "express";
 import cors from 'cors';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
-import authRoutes from "./routes/auth.route"
+import authRoutes from "./routes/auth.route";
+import adminRoutes from "./routes/admin.route";
+import inventoryRoutes from "./routes/inventory.route";
+import shopRoutes from "./routes/shop.route";
 
+import { authenticate, authorizeAdmin } from "./middleware/auth.middleware";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const port = 5000
+const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/auth', authRoutes)
+app.use('/api/auth', authRoutes);
+app.use('/api/admin', authenticate, authorizeAdmin, adminRoutes);
+app.use('/api/inventory', authenticate, inventoryRoutes);
+app.use('/api/shop', authenticate, shopRoutes);
 
 app.listen(port, () => {
-    console.log(`listening on port ${port}`)
-})
+    console.log(`listening on port ${port}`);
+});
